3182b595926158bd6f5a1763127d70e7c7bfe47c
Kumar <sahil.kumar@esko.com>
2025-03-13 11:26:56 +0530

    added query in theater repository to get cities

--- makemyshow/src/main/java/com/example/makemyshow/repository/TheaterRepository.java
+++ makemyshow/src/main/java/com/example/makemyshow/repository/TheaterRepository.java
@@ -2,6 +2,7 @@ package com.example.makemyshow.repository;
 
 import com.example.makemyshow.model.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
+    @Query("SELECT DISTINCT t.city FROM Theater t WHERE t.isActive = true")
     List<String> findDistinctCityByIsActiveTrue();
     List<Theater> findByCityAndIsActiveTrue(String city);
     List<Theater> findByOwnerId(Long ownerId);
